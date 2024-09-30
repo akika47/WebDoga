2d0fb1f584af639558246eeef9be88e6bc697599
otvos.akos <akika02531@gmail.com>
2024-09-30 11:29:49 +0200

    4-es feladat kesz

--- EpitmenyadoWebApp/Pages/AdoOsszeg.cshtml.cs
+++ EpitmenyadoWebApp/Pages/AdoOsszeg.cshtml.cs
@@ -1,49 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using EpitmenyadoWebApp.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using EpitmenyadoWebApp.Models;
 
-namespace EpitmenyadoWebApp.Pages
+public class AdoOsszegModel : PageModel
 {
-    public class AdoOsszegModel : PageModel
+    private readonly EpitmenyadoWebApp.Models.EpitmenyDbContext _context;
+
+    public AdoOsszegModel(EpitmenyadoWebApp.Models.EpitmenyDbContext context)
     {
-        private readonly EpitmenyadoWebApp.Models.EpitmenyDbContext _context;
+        _context = context;
+    }
 
-        public AdoOsszegModel(EpitmenyadoWebApp.Models.EpitmenyDbContext context)
+    public IList<Telkek> Telkek { get; set; } = default!;
+    public Dictionary<int, int> TotalTaxes { get; set; } = new Dictionary<int, int>();
+
+    public async Task OnGetAsync()
+    {
+        Telkek = await _context.Telkeks.ToListAsync();
+        var adoszamok = Telkek.GroupBy(x => x.AdoSzam).ToArray();
+        foreach (var item in adoszamok)
         {
-            _context = context;
-        }
-
-        public IList<Telkek> Telkek { get;set; } = default!;
-
-        public async Task OnGetAsync()
-        {
-            Telkek = await _context.Telkeks.ToListAsync();
-            var adoszamok = Telkek.GroupBy(x => x.AdoSzam).ToArray();
             int savAdo = 0;
-            foreach (var item in adoszamok)
+            var sav = Telkek.Where(x => x.AdoSzam == item.Key).Select(x => x.Sav);
+            if (sav.All(x => x == 'A'))
             {
-                var sav = Telkek.Where(x => x.AdoSzam == item.Key).Select(x => x.Sav);
-                if (Convert.ToChar(sav) == 'A')
-                {
-                    savAdo = 800;
-                }
-                else if (Convert.ToChar(sav) == 'B')
-                {
-                    savAdo = 600;
-                }
-                else if (Convert.ToChar(sav) == 'C')
-                {
-                    savAdo = 100;
-                }
-                int adoOsszeg = Convert.ToInt32(Telkek.Where(x => x.AdoSzam == item.Key).Select(x => x.AlapTerulet)) + savAdo ;
+                savAdo = 800;
             }
+            else if (sav.All(x => x == 'B'))
+            {
+                savAdo = 600;
+            }
+            else if (sav.All(x => x == 'C'))
+            {
+                savAdo = 100;
+            }
+            int adoOsszeg = Convert.ToInt32(Telkek.Where(x => x.AdoSzam == item.Key).Sum(x => x.AlapTerulet)) * savAdo;
+            if (adoOsszeg < 10000)
+            {
+                adoOsszeg = 0;
+            }
+            TotalTaxes.Add(item.Key, adoOsszeg);
         }
-
-
     }
 }
